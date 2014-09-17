ce63399c1545bdfefe416ccfe4fbf6ccac4eee1a
SuperStrandby <strandby@live.dk>
2014-09-17 13:24:28 +0200

    test

---
+++ producer-consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace producer_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoundedBuffer buff = new BoundedBuffer(4);
+
+            Producer prod = new Producer(buf, 15);
+
+            Consumer con = new Consumer(buf);
+
+            Parallel.Invoke(prod.Run(), con.Run());
+        }
+    }
+}
